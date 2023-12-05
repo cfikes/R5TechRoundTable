8819837aec9fe331dacc69066a73ebb8fde80a8e
Christopher Fikes <cfikes@fikesmedia.com>
2023-12-05 09:54:34 -0600

    NewScript

--- Powershell Scripts/Speak.ps1
+++ Powershell Scripts/Speak.ps1
@@ -1,15 +1,24 @@
+param (
+    [Parameter (Position=0,Mandatory = $True,ValueFromPipeline=$true)][string]$Sentence,
+    [int]$Voice=0
+)
+
 function speak {
 
     [CmdletBinding()]
     param (	
-    [Parameter (Position=0,Mandatory = $True,ValueFromPipeline=$true)]
-    [string]$Sentence
+    [string]$Sentence,
+    [int]$Voice
     ) 
     
-    $s.Voice = $s.GetVoices().Item(0)
+    
     $s=New-Object -ComObject SAPI.SpVoice
+    $s.Voice = $s.GetVoices().Item($Voice)
     $s.Rate = -2
     $s.Speak($Sentence)    
 }
 
-speak "Hello World"
+if (($Voice -eq 0) -or ($Voice -eq 1))
+{
+    speak $Sentence -Voice $Voice
+}
